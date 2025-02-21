532687a255b08d62838c231155ea9d3ebba1d56e
israt jahan akhi <isratjahanakhi715@gmail.com>
2025-02-21 20:26:03 +0600

    change in ActorGround and Main
    update the speed

--- core/src/main/java/actors/ActorGround.java
+++ core/src/main/java/actors/ActorGround.java
@@ -7,41 +7,38 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class ActorGround extends Actor {
     private Texture groundTexture;
-    private float scrollX, speed;
+    private float scrollX; private float speed;
 
-    public ActorGround(float speed) {
-        this.groundTexture = new Texture((Gdx.files.internal("images/ground.png")));
-        this.groundTexture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.ClampToEdge);
-        this.scrollX = 0;
-        this.speed = 2;
-        this.setSize(Gdx.graphics.getWidth(), this.groundTexture.getHeight());
-        this.setPosition(0, 0);
+    public ActorGround(float groundSpeed) {
+        groundTexture = new Texture(Gdx.files.internal("images/ground.png"));
+        groundTexture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.ClampToEdge);
+        scrollX = 0;
+        speed = groundSpeed; // Ground speed dynamically pass kora
+        setSize(Gdx.graphics.getWidth(), groundTexture.getHeight());
+        setPosition(0, 0);
     }
 
     @Override
     public void act(float delta) {
-        this.scrollX += this.speed;
-        if (this.scrollX > this.groundTexture.getWidth()) {
-            this.scrollX -= this.groundTexture.getWidth();
+        scrollX += speed * delta;
+        if (scrollX > groundTexture.getWidth()) {
+            scrollX -= groundTexture.getWidth();
         }
     }
-
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        int textureWidth = this.groundTexture.getWidth();
-        int screenWidth = (int) this.getWidth();
 
-        batch.draw(this.groundTexture, this.getX() - this.scrollX, this.getY(), textureWidth, this.getHeight());
-        batch.draw(this.groundTexture, this.getX() - this.scrollX + textureWidth, this.getY(), textureWidth, this.getHeight());
-
-        System.out.println(this.getX());
+        int textureWidth = groundTexture.getWidth();
+        int screenWidth = (int) getWidth();
+        batch.draw(groundTexture, getX() - scrollX, getY(), textureWidth, getHeight());
+        batch.draw(groundTexture, getX() - scrollX + textureWidth, getY(), textureWidth, getHeight());
     }
-
-    // Manually dispose method
     public void disposeTexture() {
-        if (this.groundTexture != null) {
-            this.groundTexture.dispose();
+        if (groundTexture != null) {
+            groundTexture.dispose();
         }
     }
+
+
 }
